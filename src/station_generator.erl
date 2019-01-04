1062708591d14cf37442063992c1344ed2765668
bartoszgaj <bartosz.gaj4@gmail.com>
2019-01-04 12:12:32 +0100

    Added PlatformsView generating and creating a list

--- src/station_generator.erl
+++ src/station_generator.erl
@@ -2,16 +2,16 @@
 -export([generate_train/0, generate_platforms/0]).
 -import(station, [start/0, add_platform/1, add_train/2]).
 
-for(0,_) -> 
-   []; 
-   
-for(N,Term) when N > 0 -> 
-   station:add_platform(N), 
-   [Term|for(N-1,Term)]. 
+for(0,_) ->
+   [];
+
+for(N,Term) when N > 0 ->
+   station:add_platform(N),
+   [Term|for(N-1,Term)].
 
 %generuje losowa ilosc peronow 0-6
 platformNo() ->
-    crypto:rand_uniform(1,6). 
+    crypto:rand_uniform(1,6).
 
 %wybiera i dodaje losowa ilosc peronow z przedzialu 0-6
 generate_platforms() ->
@@ -35,7 +35,6 @@ randchar(N, Acc) ->
 trainName() ->
     randchar(4).
 
-%generuje pociag o losowej nazwie i czasie    
+%generuje pociag o losowej nazwie i czasie
 generate_train() ->
     station:add_train(trainName(), trainTime()).
-    
--- src/station_server.erl
+++ src/station_server.erl
@@ -1,5 +1,6 @@
 -module(station_server).
 -compile(export_all).
+-include_lib("wx/include/wx.hrl").
 %-include_lib("wx/include/wx.hrl").
 -import(station, [start/0]).
 -import(station_generator, [generate_train/0, generate_platforms/0]).
@@ -15,7 +16,7 @@ print({clear}) ->
     io:format("\e[2J", []).
 
 printxy({X,Y,Msg}) ->
-   io:format("\e[~p;~pH~p~n",[Y,X,Msg]).  
+   io:format("\e[~p;~pH~p~n",[Y,X,Msg]).
 
 %init the app
 init() ->
@@ -26,11 +27,35 @@ init() ->
     print({printxy, 6, 8, "station_server:auto(): TRYB AUTOMATYCZNY"}),
     print({printxy, 6, 10, "station_server:user(): TRYB UZYTKOWNIKA"}),
     print({gotoxy, 10, 13}),
-    io:format("\n",[]).
+    io:format("\n",[]),
+    Wx = make_window().
+
+  make_window() ->
+      Server = wx:new(),
+      Frame = wxFrame:new(Server, -1, "Train station simulation", [{size,{1000,500}}]),
+      End_Button = wxButton:new(Frame, ?wxID_STOP, [{label, "End simulation"}, {pos, {300,70}}]),
+      Platform6 = [{6, wxStaticText:new(Frame, 0, "Peron 6", [{pos, {200, 350}}])}],
+      Platform5 = [{5, wxStaticText:new(Frame, 0, "Peron 5", [{pos, {200, 300}}])}|Platform6],
+      Platform4 = [{4, wxStaticText:new(Frame, 0, "Peron 4", [{pos, {200, 250}}])}|Platform5],
+      Platform3 = [{3, wxStaticText:new(Frame, 0, "Peron 3", [{pos, {200, 200}}])}|Platform4],
+      Platform2 = [{2, wxStaticText:new(Frame, 0, "Peron 2", [{pos, {200, 150}}])}|Platform3],
+      Platform1 = [{1, wxStaticText:new(Frame, 0, "Peron 1", [{pos, {200, 100}}])}|Platform2],
+      PlatformsView = Platform1,
+
+
+      wxFrame:createStatusBar(Frame),
+      wxFrame:show(Frame),
+
+      wxFrame:connect(Frame, close_window),
+      wxButton:connect(End_Button, command_button_clicked),
+      io:format("TEST"),
+      % {Server, Frame, End_Button, Time_Text, ClientsR, ClientsS}.
+      {Server, Frame, End_Button, PlatformsView}.
+
 
 loop() ->
     station_generator:generate_train(),
-    timer:apply_after(3000, ?MODULE, loop, []).  
+    timer:apply_after(3000, ?MODULE, loop, []).
 %tu sie powinno odpalac tez gui
 %tryb automatyczny: generuje losowa ilosc peronow z przedzialu 0-6
 %oraz co sekunde generuje pociag o losowej 4-literowej nazwie i losowym czasie
@@ -44,5 +69,3 @@ auto() ->
 user() ->
     {ok, [X]} = io:fread("Number of platforms: ","~d"),
     X.
-    
-
